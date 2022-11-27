f0e6f4a4c567ad697f857d3f3f1d39f2da9115f5
rohan chaudary <rohan chaudary@DESKTOP-0IUG00F>
2022-11-27 14:42:10 +0530

    first commit

---
+++ log.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/e13e8802f2.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="login.css">
+    <title>Login to Vastram</title>
+</head>
+<body>
+
+    <div class="logo">
+        <img src="VASTRAM10.png" alt="logo">
+    </div>
+
+
+    <div class="container">
+        <h1>Login to Vastram</h1>
+        <br>
+        <div class="box">
+            <i class="fa fa-envelope-o"></i>
+            <input type="email" name="email" id="email" placeholder="Enter your Email">
+
+        </div>
+        <div class="box">
+            <i class="fa fa-key"></i>
+            <input type="password" name="password" id="password" placeholder="Enter your password">
+
+        </div>
+        <br>
+
+        <button class="btn">Sign In</button>
+    </div>
+    
+</body>
+</html>
